604c77e73716c6066f84b4dd7cc814c225949715
Mendez <mmendez33@cps.k12.il.us>
2024-10-03 09:34:50 -0500

    week6

--- sets_tuplets.py
+++ sets_tuplets.py
@@ -89,50 +89,50 @@ set4 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5}  # duplicate elements are removed
 
 
 
-#######################tuples challenge#####################
-# Challenge: Count the number of occurrences of the character 'v' in the text below.
-# The text is converted to a tuple of characters and the target characters are 'v' and 'V'.
-# The result is output to the console.
-#queue the videos(2)
-text = """Voilà! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate.
-This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation
-of a by-gone vexation stands vivified, and has vowed to vanquish these venal and virulent vermin vanguarding vice and
-vouchsafing the violently vicious and voracious violation of volition.
+# #######################tuples challenge#####################
+# # Challenge: Count the number of occurrences of the character 'v' in the text below.
+# # The text is converted to a tuple of characters and the target characters are 'v' and 'V'.
+# # The result is output to the console.
+# #queue the videos(2)
+# text = """Voilà! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate.
+# This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation
+# of a by-gone vexation stands vivified, and has vowed to vanquish these venal and virulent vermin vanguarding vice and
+# vouchsafing the violently vicious and voracious violation of volition.
 
 
-The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day
-vindicate the vigilant and the virtuous.
+# The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day
+# vindicate the vigilant and the virtuous.
 
 
-Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it is my very good honor to meet you
-and you may call me V."""
+# Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it is my very good honor to meet you
+# and you may call me V."""
 
 
-# Convert the text to a tuple of characters
+# # Convert the text to a tuple of characters
 
-text_tuple = tuple(text)
-print(text_tuple)
+# text_tuple = tuple(text)
+# print(text_tuple)
 
-# Tuple to store the target characters
-
-
-
+# # Tuple to store the target characters
 
 
 
-# Count occurrences of 'v' or 'V' by filtering the text_tuple
 
-print(text_tuple.count('v'))
-print(text_tuple.count('V'))
 
-#lowercase_v= text_tuple.lower()
-#print(lowercase_v)
-# Output the result
 
-small_v= text_tuple.count('v')
-capital_v= text_tuple.count('V')
-total= small_v + capital_v
-print(total)
+# # Count occurrences of 'v' or 'V' by filtering the text_tuple
+
+# print(text_tuple.count('v'))
+# print(text_tuple.count('V'))
+
+# #lowercase_v= text_tuple.lower()
+# #print(lowercase_v)
+# # Output the result
+
+# small_v= text_tuple.count('v')
+# capital_v= text_tuple.count('V')
+# total= small_v + capital_v
+# print(total)
 
 # dictionarys Accessing a Value from a Nested List###############################
 #Suppose we have a dictionary containing multiple lists as values, and you want to access a specific element from one of these lists.
@@ -144,23 +144,38 @@ sample_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(len(sample_list)) # Output: 3
 #this is called a nested list
 # Extract and print the second element from the first list
-
+print(sample_list[0][1]) #Ouput:2
+print(sample_list[1][2])
+print(sample_list[2][0])
+print(sample_list[2][2])
+print(sample_list[0][0])
+print(sample_list[2][1])
 
 sample_list_of_fruit = {"fruits": ["apple", "banana", "cherry"]}
 # Extract and print the second fruit from the list
+print(sample_list_of_fruit["fruits"][1]) #output:banana
+print(sample_list_of_fruit["fruits"][2])
+print(sample_list_of_fruit["fruits"][0])
 
 
 sample_list_of_lists = {"lists": [[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
 # Extract and print the third element from the second list
 
-
+print(sample_list_of_lists["lists"][1][2])
+print(sample_list_of_lists["lists"][2][1])
+print(sample_list_of_lists["lists"][0][2])
+print(sample_list_of_lists["lists"][2][0])
 
 
 sample_list_of_dicts = {"dicts": [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 30}, {"name": "Charlie", "age": 35}]}
 # Extract and print the age of the second person
+print(sample_list_of_dicts["dicts"][1]["age"]) #Output:30
+ 
+ #name of third person
+print(sample_list_of_dicts["dicts"][2]["name"])
 
-
-
+#age of first person
+print(sample_list_of_dicts["dicts"][0]["name"])
 
 
 
@@ -173,7 +188,11 @@ data = {
 # Extract and print the second item from the 'tropical' list
 print(data["fruits"]["tropical"][1])  # Output: 'pineapple'
 
+print(data["fruits"]["berries"][2]) #Output: raspberry
 
+print(data["prices"]["banana"]) #Output: 0.5
+
+print(data["prices"]["mango"]) #Output:1.5 
 
 
 # Define the dictionary
@@ -186,7 +205,11 @@ info = {
 # Extract and print the coach's name
 print(info["team"]["coach"]["name"])  # Output: 'John Doe'
 
+#age of coach, thirs player, location
 
+print(info["team"]["coach"]["age"])
+print(info["team"]["players"][2])
+print(info["location"])
 
 
 # Define the dictionary
@@ -206,6 +229,14 @@ company = {
 
 # Extract and print the second employee from the 'Engineering' department
 print(company["departments"]["Engineering"]["employees"][1])  # Output: 'Noah'
+
+#second budget of teh company 
+print(company["departments"]["Engineering"]["budget"]) #Ouput:120000
+
+#find total budget 
+budget1= company["departments"]["HR"]["budget"]
+budget2= company["departments"]["Engineering"]["budget"]
+print(budget1 +budget2)
 
 
 # Define the dictionary
